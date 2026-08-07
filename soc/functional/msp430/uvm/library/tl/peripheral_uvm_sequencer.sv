d5942b52be9883e791f34cf840718ae7e5f8c8a5
PacoReinaCampo <frareicam@gmail.com>
2026-08-08 00:11:37 +0400

    Working in SoC-DV 🚀

--- soc/functional/msp430/uvm/library/tl/peripheral_uvm_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class peripheral_uvm_sequencer extends uvm_sequencer #(peripheral_uvm_transaction);
-  // Declaration of component utils to register with factory
-  `uvm_component_utils(peripheral_uvm_sequencer)
-
-  // Constructor
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
